72259f37d163adad830398ff70df55a03a6d38fb
Mohanned Mahmoud <m.mahmoud2203@nu.edu.eg>
2026-07-05 20:37:40 +0300

    first commit

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import { pinoHttp } from "pino-http";
 import router from "./routes/index.js";
 import { logger } from "./lib/logger.js";
 
